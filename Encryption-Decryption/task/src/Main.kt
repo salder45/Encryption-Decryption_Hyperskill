fc412d1b89a9f16c77a98636c38aa8933892da94
Eder Martinez <edermtz.04@gmail.com>
2023-01-23 08:34:23 -0600

    Add First Stage solution

--- Encryption-Decryption/task/src/Main.kt
+++ Encryption-Decryption/task/src/Main.kt
@@ -1,5 +1,9 @@
 package encryptdecrypt
 
 fun main() {
-    println("Hello World!")
+    //A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-S-T-U-V-W-X-Y-Z
+    //Z-Y-X-W-V-U-T-S-R-Q-P-O-N-M-L-K-J-I-H-G-F-E-D-C-B-A
+    //we found a treasure!
+    //dv ulfmw z givzhfiv!
+    println("dv ulfmw z givzhfiv!")
 }
