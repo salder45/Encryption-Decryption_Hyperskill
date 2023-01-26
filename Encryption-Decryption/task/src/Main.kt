7ca25ba9f684d29d004a1a74c820f6bd3898c5b6
Eder Martinez <edermtz.04@gmail.com>
2023-01-25 20:17:16 -0600

    Finish stage 3

--- Encryption-Decryption/task/src/Main.kt
+++ Encryption-Decryption/task/src/Main.kt
@@ -4,11 +4,15 @@ fun main() {
     val operation: String = readln()
     val input: String = readln()
     val key: Int = readln().toInt()
-    if (operation == "enc") {
-        //encrypt
-        println("encrypt")
+    var output: String = ""
+    val numericOperation: Int = if (operation == "enc") {
+        1
     } else {
-        //decrypt
-        println("decrypt")
+        -1
     }
+    for (chr in input) {
+        val newValue: Int = chr.code + key * numericOperation
+        output+= newValue.toChar()
+    }
+    println(output)
 }
